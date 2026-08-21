9b5e7d0ff8fa318ce97e2889406aa89938a24a91
unictoai <2.10971849e+08+unictoai@users.noreply.github.com>
2026-08-21 12:34:17 +0000

    Bump Dora to 0.9.0 pre-alpha

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,8 +20,8 @@ android {
         applicationId = "app.dora.localai"
         minSdk = 26
         targetSdk = 35
-        versionCode = 10
-        versionName = "0.8.0-prealpha"
+        versionCode = 11
+        versionName = "0.9.0-prealpha"
 
         ndk {
             abiFilters += listOf("arm64-v8a")
